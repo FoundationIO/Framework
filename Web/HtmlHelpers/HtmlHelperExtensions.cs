57af9c91ecfc761576dc9684609ab1d0d738a3fd
Guru Kathiresan <gururamnath@yahoo.com>
2017-10-05 17:31:52 -0700

    Changes to Dropdown helper to trigger onSelect event

--- Web/HtmlHelpers/HtmlHelperExtensions.cs
+++ Web/HtmlHelpers/HtmlHelperExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Framework.Infrastructure.Models;
+using Framework.Infrastructure.Utils;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -28,7 +29,12 @@ namespace Framework.Web.HtmlHelpers
             var defaultText = string.Empty;
             var defaultValue = string.Empty;
             StringBuilder sbMenu = new StringBuilder();
-            sb.AppendLine($"<select id =\"{id}\" name=\"{id}\" class=\"{className}\" style =\"{style}\">");
+            string onChangeEvent = "";
+
+            if (callbackFunction.IsTrimmedStringNotNullOrEmpty())
+                onChangeEvent = $"onChange=\"javascript:{callbackFunction}(this);\"";
+
+            sb.AppendLine($"<select id =\"{id}\" name=\"{id}\" class=\"{className}\" style =\"{style}\"  {onChangeEvent}  >");
             foreach (var listItem in items)
             {
                 var selected = string.Empty;
@@ -42,67 +48,6 @@ namespace Framework.Web.HtmlHelpers
 
             sb.AppendLine("</select>");
             return new HtmlString(sb.ToString());
-        }
-
-        public static HtmlString BootstrapPager(this IHtmlHelper helper, int currentPageIndex, string url, long totalItems, long pageSize = 10, int numberOfLinks = 5)
-        {
-            if (totalItems <= 0)
-            {
-                return new HtmlString(string.Empty);
-            }
-
-            var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
-            var lastPageNumber = (int)Math.Ceiling((double)currentPageIndex / numberOfLinks) * numberOfLinks;
-            var firstPageNumber = lastPageNumber - (numberOfLinks - 1);
-            var hasPreviousPage = currentPageIndex > 1;
-            var hasNextPage = currentPageIndex < totalPages;
-            if (lastPageNumber > totalPages)
-            {
-                lastPageNumber = totalPages;
-            }
-
-            StringBuilder sb = new StringBuilder();
-            sb.AppendFormat("<ul class = 'pagination pagination-sm no-margin pull-right'> ");
-
-            sb.AppendLine(" " + AddLink(1, url, currentPageIndex == 1, "disabled", "&lt;&lt;", "First Page"));
-            sb.AppendLine(" " + AddLink(currentPageIndex - 1, url, !hasPreviousPage, "disabled", "&lt;", "Previous Page"));
-            for (int i = firstPageNumber; i <= lastPageNumber; i++)
-            {
-                sb.AppendLine(" " + AddLink(i, url, i == currentPageIndex, "active", i.ToString(), i.ToString()));
-            }
-
-            sb.AppendLine(" " + AddLink(currentPageIndex + 1, url, !hasNextPage, "disabled", "&gt;", "Next Page"));
-            sb.AppendLine(" " + AddLink(totalPages, url, currentPageIndex == totalPages, "disabled", "&gt;&gt;", "Last Page"));
-
-            sb.AppendLine(" </ul>");
-            return new HtmlString(sb.ToString());
-        }
-
-        private static string AddLink(int index, string url, bool condition, string classToAdd, string linkText, string tooltip)
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendFormat("<li title='{0}'", tooltip);
-
-            if (condition)
-            {
-                sb.AppendFormat(" class = '{0}'", classToAdd);
-            }
-
-            sb.AppendFormat("> ");
-
-            var pageNumString = string.Empty;
-            if (!condition)
-            {
-                if (url.Contains('?') == false)
-                    pageNumString += "?Page=" + index;
-                else
-                    pageNumString += "&Page=" + index;
-            }
-
-            sb.AppendFormat("<a href='{0}{1}'>{2}</a> ", !condition ? url : "javascript:", pageNumString, linkText);
-
-            sb.AppendFormat("</li> ");
-            return sb.ToString();
         }
     }
 }
